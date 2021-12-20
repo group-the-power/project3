f76d1ed127ae1facc3f660bbbc89c2a219fb907d
ghada1999 <Ghada.Alshamrani.eam.js@tuwaiq.ed>
2021-12-20 16:04:31 +0300

    controllers

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -4,6 +4,7 @@ const adminSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   Password: { type: String, required: true },
+  city: { type: String, required: true },
 });
 
 module.exports.Admin = mongoose.model("Admin", adminSchema);
