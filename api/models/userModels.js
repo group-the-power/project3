76c459c78a9567cab0639130a487977f8f5a9433
NadaAlraghieb <Nada.Alraghieb.eam.js@tuwaiq.edu.sa>
2021-12-20 11:33:33 +0300

    new Schema

---
+++ api/models/userModels.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  Password: { type: String, required: true },
+});
+
+module.exports.Admin = mongoose.model("Admin", adminSchema);
